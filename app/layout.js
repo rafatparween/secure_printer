ef4d32993091482338a378602c257282160fd1a6
rafatparween <rafatparween27392@gmail.com>
2026-02-17 21:25:10 +0530

    try to fix an error

--- app/layout.js
+++ app/layout.js
@@ -68,7 +68,6 @@
 // }
 
 
-
 import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script";
 import "./globals.css";
@@ -96,30 +95,22 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* Google Ads gtag.js */}
+
+        {/* Google Tag (Only This One) */}
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=AW-17886206014"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17935270639"
           strategy="afterInteractive"
         />
 
-        {/* Google Ads init + debug logs */}
-        <Script id="google-ads-gtag" strategy="afterInteractive">
+        <Script id="google-tag" strategy="afterInteractive">
           {`
-            console.log("🟡 Loading Google Ads gtag...");
-
             window.dataLayer = window.dataLayer || [];
-
-            function gtag() {
-              console.log("🔥 gtag called with:", arguments);
-              dataLayer.push(arguments);
-            }
-
+            function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'AW-17886206014');
-
-            console.log("✅ Google Ads gtag initialized successfully");
+            gtag('config', 'AW-17935270639');
           `}
         </Script>
+
       </head>
 
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
@@ -132,4 +123,3 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
-
